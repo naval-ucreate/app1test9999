4876325c40d9e2f5c3fbcb0debe72f222fb0789f
ucreate-74 <naval@ucreate.co.in>
2019-01-31 16:45:29 +0530

    test php info

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
+phpinfo();
 /*
 * @help : https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-php
 https://docs.jelastic.com/connection-to-postgresql-for-php
 http://tripal.info/tutorials/v3.x/installation/server-setup/ubuntu-18.04
-*/
+
 require_once('config.php');
 
 $res = testConfig();
 echo $res;
-/*
+
 $conn = pgSqlConnect();
 $createTable = "CREATE TABLE IF NOT EXISTS  emp_data(
     name text,
@@ -20,8 +21,8 @@ $conn   = pgSqlConnect();
 $result = pg_query($conn, $createTable);
 
 
-//$insertQuery    = "insert into emp_data(name,age,designation,salary) values('Ashok',12,'test',12000)";
-//$result         = pg_query($conn, $insertQuery );
+$insertQuery    = "insert into emp_data(name,age,designation,salary) values('Ashok',12,'test',12000)";
+$result         = pg_query($conn, $insertQuery );
 
 
 $query            = "select * from emp_data";
@@ -31,6 +32,6 @@ while ($data = pg_fetch_array($result))
   print_R($data);
 }
 
-
+*/
 
 
