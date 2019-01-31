d45e2ba6c442a85ddf73c6a013c44f87b30f9cd9
ucreate-74 <naval@ucreate.co.in>
2019-01-31 17:04:13 +0530

    test db connection on heroku server

--- index.php
+++ index.php
@@ -7,9 +7,6 @@ http://tripal.info/tutorials/v3.x/installation/server-setup/ubuntu-18.04
 */
 ini_set("display_errors", "1");
 error_reporting(E_ALL);
-
-echo DATABASE_URL;
-/*
 require_once('config.php');
 $res    = testConfig();
 echo    $res;
@@ -31,6 +28,5 @@ while ($data = pg_fetch_array($result))
   print_R($data);
 }
 
-*/
 
 
